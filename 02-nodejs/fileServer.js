0bc8e818d59e1d646a9dad72a024f1d725684802
Sundaram Sharma <sundaram-sharma@hotmail.com>
2023-07-15 18:24:35 +0530

    file server completed

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -22,8 +22,27 @@ const path = require('path');
 const app = express();
 
 app.get("/files", (req, res) =>{
-  fs.readdir()
+  fs.readdir(path.join(__dirname, './files/'),(err,files)=>{
+    if(err){
+      return res.status(500).json({
+        error: 'Failed to retrive the file'
+      })
+    }
+    else{
+      res.json(files);
+    }
+  })
 })
+app.get('/file/:filename', (req, res) => {
+  const filepath = path.join(__dirname, './files/', req.params.filename);
+
+  fs.readFile(filepath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(404).send('File not found');
+    }
+    res.send(data);
+  });
+});
 
 
 app.listen(3000)
